a38c91fa7552a54cfcfcccbb9e9350b33cc3ad8c
David Andersson <David Andersson>
2019-03-27 13:48:16 +0100

    Form group validation

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, AbstractControl } from '@angular/forms';
+import { FormControl, Validators, AbstractControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,16 +7,37 @@ import { FormControl, Validators, AbstractControl } from '@angular/forms';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-	avatarNameControl: FormControl = new FormControl('DragonSlayer', [Validators.required, Validators.minLength(6)]);
-	favLanguageControl: FormControl = new FormControl('', [Validators.required, this.isKlingon]);
+	favLanguageControl = new FormControl('', [Validators.required, this.isKlingon]);
+
+
+	namesGroup: FormGroup = new FormGroup({
+		avatarNameControl: new FormControl('DragonSlayer', [Validators.required, Validators.minLength(6)]),
+		realNameControl: new FormControl('', [Validators.required])
+		},
+		{ validators: [this.sameNameValidator] }
+	);
+	realNameControl: AbstractControl;
+	avatarNameControl: AbstractControl;
 
 	constructor() { }
 
-	ngOnInit() { }
+	ngOnInit() { 
+		this.realNameControl = this.namesGroup.controls['realNameControl'];
+		this.avatarNameControl = this.namesGroup.controls['avatarNameControl'];
+	}
 
 	isKlingon(text: AbstractControl) {
 		if( text.value.toUpperCase() === 'KLINGON' ) {
 			return { isKlingon: true };
+		}
+		return null;
+	}
+	sameNameValidator(group: FormGroup) {
+		// Use get or controls, whatever you like best
+		let realName = group.get('realNameControl').value;
+		let avatarName = group.controls['avatarNameControl'].value;
+		if( realName === avatarName ) {
+			return { sameName: true };
 		}
 		return null;
 	}
